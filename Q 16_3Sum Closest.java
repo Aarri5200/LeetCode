a5cb1c25a24800f653f4b05b531912f875ec2e43
Aarri.9406 <140301362+Aarri5200@users.noreply.github.com>
2025-08-27 22:30:07 +0530

    Q 16_3Sum Closest.java

---
+++ Q 16_3Sum Closest.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+        int x = nums[0] + nums[1] + nums[2];
+
+        for(int i = 0; i < (n-2); i++){
+
+            int left = i+1;
+            int right = n-1;
+
+            while(left < right){
+
+                int sum  = nums[i] + nums[left] + nums[right];
+
+                if(Math.abs(target-sum) < Math.abs(target-x)){
+                    x = sum;
+                }
+
+                if(sum < target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+
+            }
+
+        }
+
+        return x;
+        
+    }
+}
